13bf1638c60ec6fbd94000514076a961b786599a
Vagner Alves Leite <vagnerleite@MacBook-Pro-de-Macbook-2.local>
2019-10-10 23:16:02 -0300

    criação do frontend da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnc"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-Mail *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+          />
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
